0a8e5b08c93f6021391574a8f0e4fe15b6ac8feb
ngesa marvin <marvinngesa@gmail.com>
2018-01-19 22:44:15 +0300

    turning the BLUE LED on and off at an interval

--- app/src/main/java/com/droidmarvin/digitaloutputswithrainbowhatleds/MainActivity.java
+++ app/src/main/java/com/droidmarvin/digitaloutputswithrainbowhatleds/MainActivity.java
@@ -2,18 +2,23 @@ package com.droidmarvin.digitaloutputswithrainbowhatleds;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.os.Looper;
 import android.util.Log;
 
 import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
 
 import java.io.IOException;
+import java.util.concurrent.TimeUnit;
 
 public class MainActivity extends Activity {
 
     private static final String BLUE_LED_PIN = "BCM26";
 
     private Gpio mGpio;
+
+    private Handler ledToggleHandler;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,8 +42,48 @@ public class MainActivity extends Activity {
 
         }
 
+        ledToggleHandler = new Handler(Looper.getMainLooper());
+
     }
 
+    @Override
+    protected void onStart() {
+        super.onStart();
+        ledToggleHandler.post(toggleLed);
+    }
+
+    private final Runnable toggleLed = new Runnable() {
+        @Override
+        public void run() {
+
+            boolean ledOn;
+            try {
+                ledOn = mGpio.getValue();
+            }catch (IOException e){
+                throw new IllegalStateException(BLUE_LED_PIN + " cannot be read.", e);
+            }
+
+            try {
+                if(ledOn){
+                    mGpio.setValue(false);
+                }else {
+                    mGpio.setValue(true);
+                }
+            }catch (IOException e){
+                throw new IllegalStateException (BLUE_LED_PIN + " cannot be written.", e);
+            }
+            ledToggleHandler.postDelayed(this, TimeUnit.SECONDS.toMillis(1));
+
+        }
+    };
+
+    @Override
+    protected void onStop() {
+        ledToggleHandler.removeCallbacks(toggleLed);
+        super.onStop();
+    }
+
+    //close the ledpin
     @Override
     protected void onDestroy() {
 
