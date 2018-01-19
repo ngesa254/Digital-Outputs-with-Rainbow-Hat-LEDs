529de803e811982794c07db79b91e91d9c8880e5
ngesa marvin <marvinngesa@gmail.com>
2018-01-19 21:49:39 +0300

    create an instance of PeripheralManagerService and the Blue LED

--- app/src/main/java/com/droidmarvin/digitaloutputswithrainbowhatleds/MainActivity.java
+++ app/src/main/java/com/droidmarvin/digitaloutputswithrainbowhatleds/MainActivity.java
@@ -3,12 +3,16 @@ package com.droidmarvin.digitaloutputswithrainbowhatleds;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.things.pio.PeripheralManagerService;
+
 public class MainActivity extends Activity {
+
+    private static final String BLUE_LED_PIN = "BCM26";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
-
+        PeripheralManagerService service = new PeripheralManagerService();
     }
 }
