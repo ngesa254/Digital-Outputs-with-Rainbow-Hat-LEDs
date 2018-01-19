aab57f920d7e40bdd14a5ba8e9996191ad03d0be
ngesa marvin <marvinngesa@gmail.com>
2018-01-19 21:56:55 +0300

    open a GPIO connection with the BLUE LED PIN.

--- app/src/main/java/com/droidmarvin/digitaloutputswithrainbowhatleds/MainActivity.java
+++ app/src/main/java/com/droidmarvin/digitaloutputswithrainbowhatleds/MainActivity.java
@@ -3,16 +3,30 @@ package com.droidmarvin.digitaloutputswithrainbowhatleds;
 import android.app.Activity;
 import android.os.Bundle;
 
+import com.google.android.things.pio.Gpio;
 import com.google.android.things.pio.PeripheralManagerService;
+
+import java.io.IOException;
 
 public class MainActivity extends Activity {
 
     private static final String BLUE_LED_PIN = "BCM26";
+
+    private Gpio mGpio;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
 
         PeripheralManagerService service = new PeripheralManagerService();
+
+        try {
+
+            mGpio = service.openGpio(BLUE_LED_PIN);
+
+        } catch (IOException e){
+
+            throw new IllegalStateException(BLUE_LED_PIN + "gpio bus could not open.",e);
+        }
     }
 }
